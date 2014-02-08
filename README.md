390cdeda0aefabb9d9afd09edabbb81f56dd65d4
Michael <michaelyan2@gmail.com>
2014-02-08 01:12:18 -0500

    Initial framework commit

--- README.md
+++
@@ -1,4 +0,0 @@
-Hangout
-=======
-
-An app for allowing friends to plan hangouts and vote on which events they like
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hangout::Application.config.secret_key_base = '4ab7ce04707ba0152a126bca64e90963af2ce99e9902d1a0249ef143b46e826c31a40f4e892ad964ad1aea8bf9462a2f13f8bdd700df06b5f77e759dcb6210b7'
