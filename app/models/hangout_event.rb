39a94dfd2153a62cb6db5e50458b6ff6aa9be2c7
Michael <michaelyan2@gmail.com>
2014-03-10 22:23:12 -0400

    added sass gem and changed end_time parameter

--- app/models/hangout_event.rb
+++ app/models/hangout_event.rb
@@ -9,5 +9,9 @@ class HangoutEvent
   field :name, type: String
   field :date, type: Date
   field :start_time, type: Time
-  field :endtime, type: Time
+  field :end_time, type: Time
+
+  validates :name, :date, :presence => { :message => "You must provide a name and date." }
+  validates :start_time, :numericality => { :less_than_or_equal_to => :end_time }
+  validates :end_time, :numericality => { :greater_than_or_equal_to => :start_time }
 end
